a18267faa460108f3fd68de5b59644f475c4d4b9
Mehdi <mehdi.chamiani@gmail.com>
2021-12-17 22:45:06 +0400

    Finalised

---
+++ src/data.js
@@ -0,0 +1,85 @@
+import { v4 as uuidv4 } from 'uuid';
+
+function upBeat() {
+  return [
+    {
+      name: 'Khande Hato Ghorboon',
+      cover:
+        'http://tabamusic.com/wp-content/uploads/2020/06/Mohammad-Alizadeh-Khandehato-Ghorboon.jpg',
+      artist: 'Mohammad Alizade',
+      audio:
+        'https://irsv.upmusics.com/99/Mohammad%20Alizadeh%20%7C%20Khandehato%20Ghorboon%20(320).mp3',
+      color: ['#D3EEF7', '#DA6987'],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: 'Salamati',
+      cover:
+        'http://tabamusic.com/wp-content/uploads/2021/03/Meysam-Ebrahimi-Salamati.jpg',
+      artist: 'Meisam Ebrahimi',
+      audio:
+        'https://irsv.upmusics.com/99/%20Meysam%20Ebrahimi%20%7C%20Salamati%20(320).mp3',
+      color: ['#04275A', '#015F73'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Del Be Del',
+      cover:
+        'https://nex1music.ir/upload/2021-03-13/alireza-talischi-del-be-del-2021-03-13-21-00-25.jpg',
+      artist: 'Alireza Talischi',
+      audio:
+        'https://irsv.upmusics.com/99/Alireza%20Talischi%20-%20Del%20Be%20Del%20(320).mp3',
+      color: ['#F1CB6A', '#2CEFF3'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Del Be Del',
+      cover:
+        'https://nex1music.ir/upload/2021-03-13/alireza-talischi-del-be-del-2021-03-13-21-00-25.jpg',
+      artist: 'Alireza Talischi',
+      audio:
+        'https://irsv.upmusics.com/99/Alireza%20Talischi%20-%20Del%20Be%20Del%20(320).mp3',
+      color: ['#F1CB6A', '#2CEFF3'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Del Be Del',
+      cover:
+        'https://nex1music.ir/upload/2021-03-13/alireza-talischi-del-be-del-2021-03-13-21-00-25.jpg',
+      artist: 'Alireza Talischi',
+      audio:
+        'https://irsv.upmusics.com/99/Alireza%20Talischi%20-%20Del%20Be%20Del%20(320).mp3',
+      color: ['#F1CB6A', '#2CEFF3'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Del Be Del',
+      cover:
+        'https://nex1music.ir/upload/2021-03-13/alireza-talischi-del-be-del-2021-03-13-21-00-25.jpg',
+      artist: 'Alireza Talischi',
+      audio:
+        'https://irsv.upmusics.com/99/Alireza%20Talischi%20-%20Del%20Be%20Del%20(320).mp3',
+      color: ['#F1CB6A', '#2CEFF3'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Del Be Del',
+      cover:
+        'https://nex1music.ir/upload/2021-03-13/alireza-talischi-del-be-del-2021-03-13-21-00-25.jpg',
+      artist: 'Alireza Talischi',
+      audio:
+        'https://irsv.upmusics.com/99/Alireza%20Talischi%20-%20Del%20Be%20Del%20(320).mp3',
+      color: ['#F1CB6A', '#2CEFF3'],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default upBeat;
